745ef9afb4b8d6affd3a2994db24117c56b1cb8e
320948-AAPS <147006693+320948-AAPS@users.noreply.github.com>
2024-08-27 23:32:53 +0000

    Slight changes

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ name = input("What is your name? ")
 # print(f"Hello {name}")
 
 favorite_color = input(f"Hello {name}, what is your favorite color? ")
-reasoning_for_favorite_color = input(f"Why is your favorite color {favorite_color}? ")
+reasoning_for_favorite_color = input(f"Why is your favorite color {favorite_color}? Because ")
 print(f"{name}'s favorite color is {favorite_color} because {reasoning_for_favorite_color}.")
