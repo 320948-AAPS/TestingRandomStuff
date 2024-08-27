d1d52fe9b0197ad50a2e2647acf611140031e88f
320948-AAPS <147006693+320948-AAPS@users.noreply.github.com>
2024-08-27 23:24:49 +0000

    Added favorite color things

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 name = input("What is your name? ")
-print(f"Hello {name}")
+# print(f"Hello {name}")
+
+favorite_color = input(f"Hello {name}, what is your favorite color? ")
+print(f"{name}'s favorite color is {favorite_color}.")
